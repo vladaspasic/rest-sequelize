e11d97d6db9b8c1dfa23fd6f61ed2fe844bbd584
Vladimir Spasic <vspasic@admins-MacBook-Pro-6.local>
2015-04-05 21:24:29 +0200

    new docs

---
+++ docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "RestSequelize.Deserializer",
+        "RestSequelize.Object",
+        "RestSequelize.Resolver",
+        "RestSequelize.RestAdapter",
+        "RestSequelize.RestService",
+        "RestSequelize.Serializer"
+    ],
+    "modules": [
+        "rest-sequelize"
+    ],
+    "allModules": [
+        {
+            "displayName": "rest-sequelize",
+            "name": "rest-sequelize",
+            "description": "Rest Sequelize namespace"
+        }
+    ]
+} };
+});
